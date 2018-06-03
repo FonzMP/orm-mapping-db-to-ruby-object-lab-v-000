dbaabe440494f621d357d5cd930f51ddea75c945
Alex Wilson <fonzmp@gmail.com>
2018-06-03 17:18:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,7 +91,7 @@ class Student
       WHERE grade = ?
     SQL
 
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end
